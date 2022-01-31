55b303877095dd94d2cc62f152c383d0edb92c42
tiagoamp <tiagoamp@gmail.com>
2022-01-31 10:40:37 -0300

    Initial commit

---
+++ src/main/java/mbean/FormularioBean.java
@@ -0,0 +1,11 @@
+package mbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "formularioBean")
+@ViewScoped
+public class FormularioBean {
+	
+
+}
